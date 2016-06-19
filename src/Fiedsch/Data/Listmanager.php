db2456ad8ef0e52d2ac29c47863a8208ca492bb3
fiedsch <fiedsch@ja-eh.at>
2016-06-19 11:19:01 +0200

    added Listmanager

---
+++ src/Fiedsch/Data/Listmanager.php
@@ -0,0 +1,155 @@
+<?php
+
+/**
+ * Listmanager: Functions typically needed when working with Lists of data,
+ * e.g. a list of email addresses ehere a common task is to find duplicates
+ * in the list or where you have two lists (a target list and a blacklist)
+ * and the task would be "find the entries from the target list that are
+ * not blacklisted).
+ *
+ * @package    data_util
+ * @author     Andreas Fieger <fiedsch@ja-eh.at>
+ * @copyright  2016 Andreas Fieger
+ * @license    MIT
+ * @link       https://github.com/fiedsch/data_util
+ */
+namespace Fiedsch\Data;
+
+class Listmanager
+{
+    /**
+     * @var array
+     */
+    protected $data;
+    
+    const CASE_ASIS = 1;
+
+    const CASE_LOWER = 2;
+
+    const CASE_UPPER = 3;
+
+    /**
+     * @var int
+     */
+    protected $use_case;
+
+    /**
+     * Listmanager constructor.
+     *
+     * @param array $data
+     * @param int $use_ase
+     */
+    public function __construct(array $data, $use_case = self::CASE_ASIS)
+    {
+        $this->use_case = $use_case;
+        $this->data = self::fitCase($data, $use_case);
+    }
+
+    /**
+     * All entries in $this->data that are not contained in $other.
+     *
+     * @param array $other
+     * @return array
+     */
+    public function without(array $other)
+    {
+        return self::reindex(array_diff($this->data, self::fitCase($other, $this->use_case)));
+    }
+
+    /**
+     * Intersect $this->data with $other. The result only contains
+     * the values that are contained in both lists.
+     *
+     * @param array $other
+     * @return array
+     */
+    public function intersect(array $other)
+    {
+        return self::reindex(array_intersect($this->data, self::fitCase($other, $this->use_case)));
+    }
+
+    /**
+     * Union. All entries that are contained in $this->data or $other.
+     *
+     * @param array $other B in the description above
+     * @return array
+     */
+    public function or (array $other)
+    {
+        return self::reindex(array_unique(array_merge($this->data, self::fitCase($other, $this->use_case))));
+    }
+
+    /**
+     * $this->data without duplicates.
+     *
+     * @return array
+     */
+    public function unique()
+    {
+        return self::reindex(array_unique($this->data));
+    }
+
+    /**
+     * Duplicates in $this->data. Note: the first occurrence of
+     * an entry is not considered a duplicate and thus not contained
+     * in the result. Example [1,2,1,2,2,1] yields [1,2,2,1].
+     *
+     * @return array
+     */
+    public function duplicates()
+    {
+        return self::reindex(array_diff_key($this->data, array_unique($this->data)));
+    }
+
+    /**
+     * Reorganize $list such that the entries are indexed
+     * from 0, 1, ...
+     *
+     * @param array $list
+     */
+    public static function reindex(array $list)
+    {
+        return array_values($list);
+    }
+
+    /**
+     * @param array $data
+     * @param int $use_ase
+     */
+    public static function fitCase(array $data, $use_case = self::CASE_ASIS)
+{
+    switch ($use_case) {
+        case self::CASE_ASIS: return $data; break;
+        case self::CASE_LOWER: return self::toLowerCase($data); break;
+        case self::CASE_UPPER: return self::toUpperCase($data); break;
+        default: throw new \RuntimeException("invalid value for use_case: '$use_case'");
+    }
+}
+
+/**
+ * Return the list where all entries are transformed to lower case.
+ * @param array $list
+ * @return array
+ */
+public
+static function toLowerCase(array $list)
+{
+    return array_map(function ($element) {
+        return mb_strtolower($element);
+    }, $list);
+}
+
+/**
+ * Return the list where all entries are transformed to lower case.
+ * @param array $list
+ * @return array
+ */
+public
+static function toUpperCase(array $list)
+{
+    return array_map(function ($element) {
+        return mb_strtoupper($element);
+    }, $list);
+}
+
+}
---
+++ tests/Fiedsch/Data/ListmanagerTest.php
@@ -0,0 +1,94 @@
+<?php
+
+use Fiedsch\Data\Listmanager;
+
+/**
+ * Class LismanagerTest
+ */
+class ListmamanagerTest extends PHPUnit_Framework_TestCase
+{
+
+    /**
+     * @@expectedException \RuntimeException
+     */
+    public function testException()
+    {
+        throw new \RuntimeException("fixme Test implementieren");
+    }
+
+    public function testWithout()
+    {
+        $listA = ['a','b','c'];
+        $listB = ['c','d','e'];
+        $manager = new Listmanager($listA);
+        $this->assertEquals($manager->without($listB), ['a', 'b']);
+    }
+
+    public function testIntersect()
+    {
+        $listA = ['a','b','c'];
+        $listB = ['c','d','e'];
+        $manager = new Listmanager($listA);
+        $this->assertEquals($manager->intersect($listB), ['c']);
+        $this->assertEquals($manager->intersect($listA), $listA);
+        $listC = ['x','y','z'];
+        $this->assertEquals($manager->intersect($listC), []);
+    }
+
+    public function testOr()
+    {
+        $listA = ['a','b','c'];
+        $listB = ['c','d','e'];
+        $manager = new Listmanager($listA);
+        $this->assertEquals($manager->or($listB), ['a','b','c','d','e']);
+        $this->assertEquals($manager->or($listA), $listA);
+    }
+
+    public function testUnique()
+    {
+        $listA = ['a','b','c','c','b'];
+        $manager = new Listmanager($listA);
+        $this->assertEquals($manager->unique(), ['a','b','c']);
+    }
+
+    public function testDuplicates()
+    {
+        $listA = ['a','b','a','a','c'];
+        $manager = new Listmanager($listA);
+        $this->assertEquals($manager->duplicates(), ['a','a']);
+    }
+
+    public function testReindex()
+    {
+        $listA = [1=>'a',3=>'b',4=>'a',7=>'a',11=>'c'];
+        $this->assertEquals(Listmanager::reindex($listA), ['a','b','a','a','c']);
+    }
+
+    public function testFitCase()
+    {
+        $listA = ['a','b','A','B'];
+        $this->assertEquals(Listmanager::fitCase($listA), ['a','b','A','B']);
+        $this->assertEquals(Listmanager::fitCase($listA, Listmanager::CASE_ASIS), ['a','b','A','B']);
+        $this->assertEquals(Listmanager::fitCase($listA, Listmanager::CASE_LOWER), ['a','b','a','b']);
+        $this->assertEquals(Listmanager::fitCase($listA, Listmanager::CASE_UPPER), ['A','B','A','B']);
+    }
+
+    public function testToLowerCase()
+    {
+        $listA = ['a','B','A','a','C','@','Μ'];
+        // Note Μ (above) is the unicode upper case version of μ which looks
+        // like the  unicode upper case version of m but (of course) is a
+        // different caracter!
+        $this->assertEquals(Listmanager::toLowerCase($listA), ['a','b','a','a','c','@','μ']);
+    }
+
+    public function testToUpperCase()
+    {
+        $listA = ['a','B','A','a','C','@','µ'];
+        // Note Μ (below) is the unicode upper case version of μ which looks
+        // like the  unicode upper case version of m but (of course) is a
+        // different caracter!
+        $this->assertEquals(Listmanager::toUpperCase($listA), ['A','B','A','A','C','@','Μ']);
+    }
+
+}
